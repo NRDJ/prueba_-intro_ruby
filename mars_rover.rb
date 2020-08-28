6b28db63147fce9555bc681dba892e6962b46c57
Nicolas Riedel <nriedeldj@gmail.com>
2020-08-28 10:58:56 -0400

    Added html page builder iterating over images

--- mars_rover.rb
+++ mars_rover.rb
@@ -10,27 +10,43 @@ end
 
 def build_web_page(photo_data)
 
+    head = 
+    "<!doctype html>
+    <html lang='en'>
+        <head>
+            <title>Mars Rover Photos</title>
+            <!-- Required meta tags -->
+            <meta charset='utf-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+                <!-- Bootstrap CSS -->
+            <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MDdH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+        </head>
+        <body>
+            <h1 class='text-center'>Mars Rovers Photos</h1>
+            <ul>\n"
+
+    body = ''
     photo_data.each do |photos, info|
         puts "Key: #{photos}"
         info.each_with_index do |item,index|
-            puts "Image: #{item['img_src']}" if index == 0
-        #     body +=             
-        #     "\n\t\t\t<ul>    
-        #     <li><img src='#{item['img_src']}/398380645PRCLF0030000CCAM04010L1.PNG'></li>    
-        #     <li><img src='.../398381687EDR_F0030000CCAM05010M_.JPG'></li>
-        # </ul>"
+            body += "\t\t\t\t<li><img src='#{item['img_src']}'></li>\n"
         end
     end
+
+    foot = "\t\t\t</ul>
+            <!-- Optional JavaScript -->
+            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+            <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>
+            <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIMB07jRM' crossorigin='anonymous'></script>
+        </body>
+    </html>"
+
+    page = head + body + foot
 end
 
 
 photo_data = get_rover_photos(link, api_key)
-build_web_page(photo_data)
-# puts build_web_page
+File.write('index.html', build_web_page(photo_data))
 
 
-# def show_rover(url, api_key)
-
-#     data = get_rover_photos(url, api_key) 
-#     build_web_page(data)
-# end
